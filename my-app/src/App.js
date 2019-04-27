41a2af5168ae6f4e9cfda414dbfafbbefea01405
Nussaba Khienprasit <adobebeta1997@gmail.com>
2019-04-27 23:33:45 +0700

    props +
    เพิ่มการส่ง prop
    productItem

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Footer from "./components/footer";
 import Header from './components/header';
+import ProductItem from './components/product/ProductItem';
 
 
 class App extends Component{
@@ -8,7 +9,11 @@ class App extends Component{
     return (
     <div>
       <Header />
+      <ProductItem productName = "iphone x" unitPrice="20000"/>
+      <ProductItem productName = "ipad pro10.5 x" unitPrice="35000"/>
+      <ProductItem productName = "ipad mini x" unitPrice="19500"/>
       <Footer company="zoommo" email="adobebeta1997@gmail.com"/>
+
     </div>
   );
 }
--- my-app/src/components/footer.js
+++ my-app/src/components/footer.js
@@ -1,6 +1,6 @@
 import React from 'react'
  const Footer = (props ) => {
-     return <div>{props.company} {props.email}</div>
+     return <div>Powered by {props.company} | Contect {props.email}</div>
  }
 
  export default Footer;
