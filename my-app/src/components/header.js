826145d7524cee877e5f4f56fd6ebaa91333cd95
Nussaba Khienprasit <adobebeta1997@gmail.com>
2019-04-27 23:58:27 +0700

    State : set time , interval

--- my-app/src/components/header.js
+++ my-app/src/components/header.js
@@ -1,6 +1,28 @@
-import React from 'react'
- const Header = () => {
-     return <h1>HEADER</h1>
- }
+import React , { Component } from 'react'
+ 
+// Functioncal Component
+// const Header = () => {
+//      return <h1>HEADER</h1>
+//  }
+
+//Class Component
+class Header extends Component{
+    constructor(props){
+        super(props);
+        this.state = {date : new Date()};
+        setInterval(() => this.tick(),1000);
+    }
+
+    tick(){
+        // this.state = {date : new Date()}; เปลี่ยนแบบนี้ไม่ได้
+        this.setState({date : new Date()});
+    }
+
+    render(){
+        return (
+            <div>{this.state.date.toLocaleTimeString()}</div>
+        )
+    }
+}
 
  export default Header;
